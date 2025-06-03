cf0f63b02fafa20a85be417d6983a6f7a6ee8151
Mikarific <mikerific2@gmail.com>
2025-06-03 04:13:37 -0600

    Keep comments in css.d.ts

--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -1,5 +1,11 @@
 declare module '*.module.css' {
+	/**
+	 * Generated CSS for CSS modules
+	 */
 	export const stylesheet: string;
+	/**
+	 * Exported classes
+	 */
 	const classMap: {
 		[key: string]: string;
 	};
@@ -7,6 +13,9 @@ declare module '*.module.css' {
 }
 
 declare module '*.css' {
+	/**
+	 * Generated CSS
+	 */
 	const css: string;
 	export default css;
 }
